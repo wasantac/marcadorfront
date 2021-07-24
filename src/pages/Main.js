f41d267d0dd5761ac96a01b6be6f311c9ccb4855
wasantac <34143200+wasantac@users.noreply.github.com>
2021-07-24 01:02:04 -0500

    funcionalidad

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,9 @@
 import React,{useEffect,useState} from 'react';
 import {io} from 'socket.io-client';
+import {Container,Form, FormGroup, Label, Input,Button} from 'reactstrap';
+
+const {REACT_APP_URL} = process.env;
+
 const Main = () => {
     const [playerone,setOne] = useState("player1");
     const [p1points,setP1] = useState(0);
@@ -7,20 +11,49 @@ const Main = () => {
     const [p2points,setP2] = useState(0);
     const [socket,setSocket] = useState();
     useEffect(() => {
-        const s = io("https://marcadorback.herokuapp.com/")
+        const s = io(`${REACT_APP_URL}`)
         setSocket(s);
         return () => {
             s.disconnect();
         }
-    },[])
+    },[]);
     return (
         <div>
-            <button onClick={e => {
-                socket.emit("send-data",{
-                    p1: [playerone,p1points],
-                    p2: [playertwo,p2points],
-                });
-            }}>Boton</button>
+            <Container>
+                <Form>
+                    <FormGroup>
+                    <Label for="p1">Player 1</Label>
+                    <Input type="text" name="p1" id="p1" onChange={ e => {
+                        setOne(e.target.value)
+                    }}/>
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="p1points">Player 1 Points</Label>
+                    <Input type="number" name="p1points" id="p1points" onChange={ e => {
+                        setP1(e.target.value)
+                    }}/>
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="p2">Player 2</Label>
+                    <Input type="text" name="p2" id="p2" onChange={ e => {
+                        setTwo(e.target.value)
+                    }}/>
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="p2points">Player 2 Points</Label>
+                    <Input type="number" name="p2points" id="p2points" onChange={ e => {
+                        setP2(e.target.value)
+                    }}/>
+                    </FormGroup>
+                </Form>
+                <Button onClick={e => {
+                    socket.emit("send-data",{
+                        p1: [playerone,p1points],
+                        p2: [playertwo,p2points],
+                    });
+                }}>Boton</Button>
+            </Container>
+
         </div>
     );
 }
--- src/pages/Marcador.js
+++ src/pages/Marcador.js
@@ -1,12 +1,28 @@
 import React,{useEffect,useState} from 'react';
 import {io} from 'socket.io-client';
+import '../styles/marcador.css'
+const {REACT_APP_URL} = process.env;
 const Marcador = () => {
     const [socket,setSocket] = useState();
+    const [playerone,setOne] = useState("player1");
+    const [p1points,setP1] = useState(0);
+    const [playertwo,setTwo] = useState("player2");
+    const [p2points,setP2] = useState(0);
     useEffect(() => {
-        const s = io("https://marcadorback.herokuapp.com/")
+        const s = io(`${REACT_APP_URL}`)
         setSocket(s);
         s.on("receive-data", data => {
+            
             console.log(data)
+            try{
+                setOne(data.p1[0]);
+                setP1(data.p1[1]);
+                setTwo(data.p[0]);
+                setP2(data.p[1]);
+            } catch (err) {
+                console.log(err)
+            }
+
         })
         return () => {
             s.disconnect();
@@ -14,7 +30,36 @@ const Marcador = () => {
     },[])
     return (
         <div>
-            
+                <div className="wrapper">
+        <div className="flx separation">
+            <div className="nombre-izq">
+                <span></span>
+                <span></span>
+                <p>{playerone}</p>
+            </div>
+            <div className="paralelogramo izquierdo neon">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <p className="noskew">{p1points}</p>
+            </div>    
+        </div>
+        <div className="flx">
+            <div className="nombre-der">
+                <span></span>
+                <span></span>
+                <p>{playertwo}</p>
+            </div>
+            <div className="paralelogramo derecho neon">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <p className="noskew">{p2points}</p>
+            </div>
+        </div>
+    </div>
         </div>
     );
 }
